87803c7302ff7af966fecf11ba3c81d2f27c3b0d
pragati <itispragativerma@gmail.com>
2019-11-03 20:10:27 +0530

    added messages for error

--- resumestation/accounts/views.py
+++ resumestation/accounts/views.py
@@ -4,12 +4,34 @@ from django.contrib import auth
 
 # Create your views here.
 def signup(request):
+  if request.method == 'POST':
+    if request.POST['password'] == request.POST['confirmpassword']:
+      try:
+        user = User.objects.get(username = request.POST['username'])
+        return render(request, 'accounts/signup.html', {'error' : 'Username has been already taken'})
+      except User.DoesNotExist:
+        user = User.objects.create_user(username =request.POST['username'], password=request.POST['password'], email=request.POST['email'])
+        auth.login(request, user)
+        return redirect('home')
+    else:
+      return render(request, 'accounts/signup.html', {'error' : 'Passwords must match'})
+
+  else:
     return render (request, 'accounts/signup.html')
 
 def login(request):
+  if request.method == 'POST':
+    user = auth.authenticate(username=request.POST['username'], password=request.POST['password'], email=request.POST['email'])
+    if user is not None:
+      auth.login(request, user)
+      return redirect('home')
+    else:
+      return render(request, 'accounts/login.html', {'error': 'username or password is incorrect.'})
+  else:
     return render (request, 'accounts/login.html')
 
 def logout(request):
   if request.method == 'POST':
     auth.logout(request)
     return redirect('home')
+  
