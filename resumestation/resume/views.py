3862d6c9d8673e840dea4a7a767df6d9df9aa8d6
pragati <itispragativerma@gmail.com>
2019-11-07 15:49:34 +0530

    json dump

--- resumestation/resume/views.py
+++ resumestation/resume/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+import json
 
 # Create your views here.
 def home(request):
   return render(request, 'resume/home.html')
 
+@login_required
 def create(request):
+  if request.method == 'POST':
+    data = request.POST['data']
+    y = json.loads(data)
+    print(y)
+  
   return render(request, 'resume/create.html')
