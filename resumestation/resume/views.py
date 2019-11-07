10a74ae6992f8f59e6d48ba426e8e24436057a7f
pragati <itispragativerma@gmail.com>
2019-11-07 14:38:29 +0530

    create file added

--- resumestation/resume/views.py
+++ resumestation/resume/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
   return render(request, 'resume/home.html')
+
+def create(request):
+  return render(request, 'resume/create.html')
